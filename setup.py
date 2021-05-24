fabb9cff389b41536b47231e167ff0c873cd8c48
sinkpoint <qixiang.chen@gmail.com>
2021-05-24 19:01:53 -0400

    - corrected packages for setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ config = {
     'author_email': 'qixiang.chen@gmail.com',
     'version': '0.1',
     'install_requires': ['nibabel','numpy', 'scipy'],
-    'packages': ['gts'],
+    'packages': ['gts', 'gts.io', 'gts.meas', 'gts.maps', 'gts.tractography', 'gts.stages', 'gts.scripts'],
     'name': 'gts',
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
